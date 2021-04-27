eef40cb6b910d7bb2b6b232f66d3a809663bef70
Elver Tobo <elvertobo2017@gmail.com>
2021-04-26 19:07:33 -0500

    Se crearon el loader, el slide 1 y el slide 2

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// 'use strict';
+
+// setInterval(function(){
+//     cargaPagina();
+// }, 2000);
+
+// function cargaPagina(){
+//     document.getElementById('loader').classList.add('hide');
+// }
